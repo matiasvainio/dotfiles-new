1c1d95599bbe965426239d029100fa3c983b6785
Matias Vainio <matias.s.vainio@gmail.com>
2022-11-13 17:57:40 +0200

    started to migrate settings to lua

---
+++ nvim/lua/config.lua
@@ -0,0 +1,23 @@
+-- General settings
+
+local set = vim.opt
+
+set.expandtab = true
+set.shiftwidth = 4
+set.tabstop = 4
+set.hidden = true
+set.relativenumber = true
+set.number = true
+set.termguicolors = true
+set.hlsearch = true
+set.ignorecase = true
+set.smartcase = true
+set.mouse = a
+set.scrolloff = 8
+set.sidescrolloff = 8
+set.clipboard = unnamedplus
+
+-- Keymaps
+
+vim.g.mapleader = ' '
+
